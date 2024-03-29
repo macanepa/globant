28872805d32a57944a2159ffb9f8ed77d492a0e4
Matias Canepa <matias@selfcapital.pro>
2024-03-29 20:22:56 -0300

    feat: added ecr repositories

---
+++ IaC/ecr.tf
@@ -0,0 +1,17 @@
+resource "aws_ecr_repository" "create-backup" {
+  name                 = "create-backup"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_repository" "ingestion" {
+  name                 = "ingestion"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
